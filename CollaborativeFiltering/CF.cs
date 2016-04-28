3f73470d6d9d67f6693b6020606c937df24359ca
Jordan Fitzgibbon <jrfitzgi@cs.washington.edu>
2016-04-28 00:49:20 -0700

    before switching from array to list

---
+++ CollaborativeFiltering/CF.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollaborativeFiltering
+{
+    // Uses collaborative filtering defined in the paper:
+    // Empirical Analysis of Predictive Algorithms for Collaborative Filtering
+    // By John S. Breese, David Heckerman, Carl Kadie
+    // http://courses.cs.washington.edu/courses/csep546/16sp/psetwww/2/algsweb.pdf 
+    // 
+    // Variable names will try to match the notation in the referenced paper.
+
+    public class CF
+    {
+        private const char Delimiter = ','; 
+        private const int NumColumns = 3;
+        private const int MovieIdColumn = 0;
+        private const int UserIdColumn = 1;
+        private const int RatingColumn = 2;
+
+        // We will maintain several data structures to optimize speed over memory.
+        public double[,] TrainingData; // all of the training data 3-tuples
+        public double[,] TestingData; // all of the testing data 3-tuples
+        public ConcurrentDictionary<double, double> User_AverageRatings; // Hashtable lookup by user for their average rating
+        public ConcurrentDictionary<double, double[,]> User_Items; // Hashtable lookup by user all their rated items
+        public ConcurrentDictionary<double, double> Item_Users; // Hashtable lookup by item which users rated them
+
+
+        // Implements 2.1 Eq. 1
+        public double PredictVote(double userId, double itemId)
+        {
+            double a = userId;
+
+            // Get the mean vote for a
+            double mean_v_a = 0;
+
+            // Iterate through each user in the collaborative filtering database
+            double sum = 0; // summation from the equation
+            double k = 0; // normalizing factor
+            double w = 0; // weight w(a,i)
+            double i = 0; // the other users we are iterating through
+            double mean_v_i = 0; // the other users mean vote
+            double v_ij = 0; // the other users rating for item j
+            // loop
+
+                w = this.GetWeight(a, i);
+                k += Math.Abs(w);
+
+                // get mean_v_i
+                // get user i's rating for movie j
+
+                sum += w * (v_ij - mean_v_i);
+                
+
+            // endloop
+
+            double p_aj = mean_v_a - (k * sum);
+            return p_aj;
+        }
+
+        // Implements 2.1 Eq. 2
+        public double GetWeight(double activeUserId, double otherUserId)
+        {
+            double a = activeUserId;
+            double i = otherUserId;
+
+            // Get all items common between both users
+
+            // Calculate activeUser (a) mean over the common items
+
+            // Calculate otherUser (i) mean over the common items
+
+            // Iterate over all common items
+            // loop
+                
+                // Calculate A = v_a - mean_a
+                // Calculate I = v_i - mean_i
+                // Get the product A and I and of these two and add it to a running sum (NUMER)
+                // Get the A^2 and I^2 and add to a running sum (DENOM_A and DENOM_I)
+
+            // endloop
+
+            double weight = 0.0;
+            // WEIGHT = NUMER / SQRT(DENOM_A * DENOM_I)
+
+            // TODO: remove
+            if (weight == 0)
+            {
+            }
+            else if (weight < 0)
+            {
+            }
+
+            return weight;
+        }
+
+        public void LoadData(string trainingSetPath, string testingSetPath)
+        {
+            // Populate the other data structures of training data
+            List<double[]> TrainingData = new List<double[]>();
+            this.LoadData(trainingSetPath, out TrainingData);
+            this.User_AverageRatings = new ConcurrentDictionary<double, double>();
+            this.User_Items = new ConcurrentDictionary<double, double[,]>();
+            this.Item_Users = new ConcurrentDictionary<double, double>();
+
+            long sum = 0;
+            for (int i = 0; i < TrainingData.Count(); i++)
+            {
+                sum++;
+            }
+            Console.WriteLine(sum);
+
+            this.LoadData(testingSetPath, out TrainingData);
+        }
+
+        private void LoadData(string filePath, out List<double[]> data)
+        {
+            //int numLines = 0;
+
+            //// Do one pass of the file to figure out how many examples we will need to allocate for
+            //using (StreamReader sr = File.OpenText(filePath))
+            //{
+            //    while (sr.ReadLine() != null)
+            //    {
+            //        numLines++;
+            //    }
+            //}
+
+            // Load the data into an array
+            data = new List<double[]>();
+            int i = 0;
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                string s = String.Empty;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    string[] sParts = s.Split(CF.Delimiter);
+                    double[] item = new double[3];
+                    item[CF.MovieIdColumn] = Double.Parse(sParts[CF.MovieIdColumn]);
+                    item[CF.UserIdColumn] = Double.Parse(sParts[CF.UserIdColumn]);
+                    item[CF.RatingColumn] = Double.Parse(sParts[CF.RatingColumn]);
+                    data.Add(item);
+                    i++;
+                }
+            }
+
+        }
+
+
+        //public void LoadData(string trainingSetPath, string testingSetPath)
+        //{
+        //    // Populate the other data structures of training data
+        //    this.LoadData(trainingSetPath, out this.TrainingData);
+        //    this.User_AverageRatings = new ConcurrentDictionary<double, double>();
+        //    this.User_Items = new ConcurrentDictionary<double, double[,]>();
+        //    this.Item_Users = new ConcurrentDictionary<double, double>();
+
+        //    long sum = 0;
+        //    for (int i = 0; i < this.TrainingData.GetLength(0); i++)
+        //    {
+        //        sum++;
+        //    }
+        //    Console.WriteLine(sum);
+
+        //    this.LoadData(testingSetPath, out this.TestingData);
+        //}
+
+        //private void LoadData(string filePath, out double[,] data)
+        //{
+        //    int numLines = 0;
+
+        //    // Do one pass of the file to figure out how many examples we will need to allocate for
+        //    using (StreamReader sr = File.OpenText(filePath))
+        //    {
+        //        while (sr.ReadLine() != null)
+        //        {
+        //            numLines++;
+        //        }
+        //    }
+
+        //    // Load the data into an array
+        //    data = new double[numLines, CF.NumColumns];
+        //    int i = 0;
+        //    using (StreamReader sr = File.OpenText(filePath))
+        //    {
+        //        string s = String.Empty;
+        //        while ((s = sr.ReadLine()) != null)
+        //        {
+        //            string[] sParts = s.Split(CF.Delimiter);
+        //            data[i, CF.MovieIdColumn] = Double.Parse(sParts[CF.MovieIdColumn]);
+        //            data[i, CF.UserIdColumn] = Double.Parse(sParts[CF.UserIdColumn]);
+        //            data[i, CF.RatingColumn] = Double.Parse(sParts[CF.RatingColumn]);
+        //            i++;
+        //        }
+        //    }
+
+        //}
+
+    }
+}
--- CollaborativeFiltering/Program.cs
+++ CollaborativeFiltering/Program.cs
@@ -10,6 +10,23 @@ namespace CollaborativeFiltering
     {
         public static void Main(string[] args)
         {
+            // Default training and testing files
+            string trainingSetPath = @"C:\coding\ml\data\netflix_data\TrainingRatings.txt";
+            string testingSetPath = @"C:\coding\ml\data\netflix_data\TestingRatings.txt";
+
+            if (args.Length >= 2)
+            {
+                trainingSetPath = args[0];
+                testingSetPath = args[1];
+            }
+
+            Console.WriteLine("trainingSetPath is {0}", trainingSetPath);
+            Console.WriteLine("testingSetPath is {0}", testingSetPath);
+
+            CF cf = new CF();
+            cf.LoadData(trainingSetPath, testingSetPath);
+            var trainingData = cf.TrainingData;
+            var testingData = cf.TestingData;
         }
     }
 }
