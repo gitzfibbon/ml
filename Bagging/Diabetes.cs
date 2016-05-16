b298b455411edeadba3fd93e90db915ecc739302
Jordan Fitzgibbon <jrfitzgi@cs.washington.edu>
2016-05-15 23:49:23 -0700

    start on populating weka instances from csv

--- Bagging/Diabetes.cs
+++ Bagging/Diabetes.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,18 +16,44 @@ namespace Bagging
     /// </summary>
     public class Diabetes
     {
-        private const char Delimiter = ' ';
+        private const char Delimiter = ',';
+        private const int NumAttributes = 9;
 
         public static void Run(string trainingSetPath, string testingSetPath, int numberOfModels)
         {
-            Diabetes.LoadTrainingData(trainingSetPath);
+            Trace.TraceInformation("Starting bagging");
+            Trace.TraceInformation("TrainingSetPath: {0}", trainingSetPath);
+            Trace.TraceInformation("TestingSetPath: {0}", testingSetPath);
+            Trace.TraceInformation("Models: {0}", numberOfModels);
+
+            Instances instances = Diabetes.LoadData(trainingSetPath);
         }
 
-        private static Instances LoadTrainingData(string trainingSetPath)
+        private static Instances LoadData(string filePath)
         {
-            Diabetes.DefineAttributes();
+            Trace.TraceInformation("Loading data from {0}", filePath);
 
-            return null;
+            Instances instances = Diabetes.DefineAttributes();
+
+            // Read in each row, set its attribute values, add it to the Instances object
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                string s = String.Empty;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Instance instance = new Instance(Diabetes.NumAttributes);
+                    instance.setDataset(instances);
+                    string[] parts = s.Split(Diabetes.Delimiter);
+
+                    Diabetes.ValueForNumberOfTimesPregnant(instance, 0, parts[0]);
+                    //instance.setValue(0, Diabetes.ValueForNumberOfTimesPregnant(parts[0]));
+
+                    instances.add(instance);
+
+                }
+            }
+
+            return instances;
         }
 
         // Define all the attributes for the diabetes dataset
@@ -35,9 +63,9 @@ namespace Bagging
 
             FastVector numberOfTimesPregnant = new FastVector();
             numberOfTimesPregnant.addElement("zero"); // 0
-            numberOfTimesPregnant.addElement("low"); // low
-            numberOfTimesPregnant.addElement("medium"); // medium
-            numberOfTimesPregnant.addElement("high"); // high
+            numberOfTimesPregnant.addElement("low"); // 1-4
+            numberOfTimesPregnant.addElement("medium"); // 5-9
+            numberOfTimesPregnant.addElement("high"); // 10+
             attributes.addElement(new weka.core.Attribute("numberOfTimesPregnant", numberOfTimesPregnant));
 
             FastVector plasmaGlucoseConcentration = new FastVector();
@@ -90,5 +118,45 @@ namespace Bagging
             Instances instances = new Instances("diabetes", attributes, 0);
             return instances;
         }
+
+        private static void ValueForNumberOfTimesPregnant(Instance instance, int attributeIndex, string inputValue)
+        {
+            int numberOfTimesPregnant = Int32.Parse(inputValue);
+
+            if (numberOfTimesPregnant <= 0)
+            {
+                instance.setValue(attributeIndex, "zero");
+            }
+            else if (numberOfTimesPregnant <= 4)
+            {
+                instance.setValue(attributeIndex, "low");
+            }
+            else if (numberOfTimesPregnant <= 9)
+            {
+                instance.setValue(attributeIndex, "medium");
+            }
+            else if (numberOfTimesPregnant >= 10)
+            {
+                instance.setValue(attributeIndex, "high");
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private static string GetValueFor(string inputValue)
+        {
+            switch (inputValue)
+            {
+                case "aaa":
+                    return "bbb";
+                case "ccc":
+                    return "ddd";
+                default:
+                    return null;
+            }
+        }
+
     }
 }
